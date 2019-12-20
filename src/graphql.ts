7d14d40c418412416e0aa3c85dccd4073a5d9935
mNutella <netcore.click@gmail.com>
2019-12-21 00:26:26 +0500

    conf: graphql

---
+++ src/graphql.ts
@@ -0,0 +1,35 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+export class CreateProfileInput {
+    name?: string;
+    city?: string;
+    type?: number;
+    info?: string;
+}
+
+export abstract class IMutation {
+    abstract createProfile(createProfileInput?: CreateProfileInput): Profile | Promise<Profile>;
+}
+
+export class Profile {
+    id?: number;
+    name?: string;
+    city?: string;
+    type?: number;
+    info?: string;
+}
+
+export abstract class IQuery {
+    abstract getProfiles(): Profile[] | Promise<Profile[]>;
+
+    abstract profile(id: string): Profile | Promise<Profile>;
+}
+
+export abstract class ISubscription {
+    abstract profileCreated(): Profile | Promise<Profile>;
+}
