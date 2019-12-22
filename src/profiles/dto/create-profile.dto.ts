700ebb9b531cd3f1ca5eb811fae1643f7a581739
mNutella <netcore.click@gmail.com>
2019-12-22 23:31:12 +0500

    chore: updated git ignor and removed dto file

--- src/profiles/dto/create-profile.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Min, IsString, IsInt } from 'class-validator'; // TODO
-import { CreateProfileInput } from '../../graphql';
-
-export class CreateProfileDto extends CreateProfileInput {
-  name: string;
-  city: string;
-  type: number;
-  info: string;
-}
