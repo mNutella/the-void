ace850c94ebd3a0058b17d07b455e5cf634cb18c
nutella <nutell@4ka.com>
2021-08-25 23:20:16 -0400

    refactor: changed indents

--- src/modules/episodes/dto/update-episode.input.ts
+++ src/modules/episodes/dto/update-episode.input.ts
@@ -1,5 +1,4 @@
 import { CreateEpisodeInput } from './create-episode.input';
 import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateEpisodeInput extends PartialType(CreateEpisodeInput) {
-}
+export class UpdateEpisodeInput extends PartialType(CreateEpisodeInput) {}
